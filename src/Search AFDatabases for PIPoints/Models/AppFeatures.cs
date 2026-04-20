4f38d87ae89cb00603bd79090071b4226f0e8182
PI Greybeard <55023680+Rick-Davin@users.noreply.github.com>
2026-04-20 15:49:20 -0700

    Implemented Claude suggestions to make more .NET 10

--- src/Search AFDatabases for PIPoints/Models/AppFeatures.cs
+++ src/Search AFDatabases for PIPoints/Models/AppFeatures.cs
@@ -1,18 +1,17 @@
-﻿namespace Search_AFDatabases_for_PIPoints.Models
-{
-    internal class AppFeatures
-    {
-        public required string AssetServer { get; set; }
-        public required string DatabaseSearchPattern { get; set; } = "*";
-        public required string OutputFolderPath { get; set; }   
-        public int AFSearchPageSize { get; set; } = 1000;
-        public bool ShowRelativePIPoints { get; set; } = false;
-        public bool ShowCurrentValue { get; set; } = false;
-        public bool ShowFirstRecorded { get; set; } = false;   // WARNING: this is a sluggish AFSDK call
-        public string? OutputWriter { get; set; } = null;    
-        public bool UseReportAutoSave { get; set; } = true;
-        public int AutoSaveSeconds { get; set; } = -1;
-        public List<string>? TagGroupingSeparators { get; set; } = null;
+﻿namespace Search_AFDatabases_for_PIPoints.Models;
 
-    }
+internal record AppFeatures
+{
+    public required string AssetServer { get; set; }
+    public required string DatabaseSearchPattern { get; set; } = "*";
+    public required string OutputFolderPath { get; set; }   
+    public int AFSearchPageSize { get; set; } = 1000;
+    public bool ShowRelativePIPoints { get; set; } = false;
+    public bool ShowCurrentValue { get; set; } = false;
+    public bool ShowFirstRecorded { get; set; } = false;   // WARNING: this is a sluggish AFSDK call
+    public string? OutputWriter { get; set; } = null;    
+    public bool UseReportAutoSave { get; set; } = true;
+    public int AutoSaveSeconds { get; set; } = -1;
+    public List<string>? TagGroupingSeparators { get; set; } = null;
+
 }
--- src/Search AFDatabases for PIPoints/Models/IReportWriter.cs
+++ src/Search AFDatabases for PIPoints/Models/IReportWriter.cs
@@ -1,16 +1,15 @@
-﻿namespace Search_AFDatabases_for_PIPoints.Models
+﻿namespace Search_AFDatabases_for_PIPoints.Models;
+
+internal interface IReportWriter
 {
-    internal interface IReportWriter
-    {
-        void CustomDispose();
-        void CustomSave();
-        void Initialize();
-        void Save(bool disposing = false);
+    void CustomDispose();
+    void CustomSave();
+    void Initialize();
+    void Save(bool disposing = false);
 
-        void WriteRows(IDictionary<AFAttribute, PIPoint> map,
-                       PIPointList tags,
-                       IDictionary<PIPoint, AFValue> firstRecordedDict,
-                       IDictionary<PIPoint, AFValue> currentValueDict);
+    void WriteRows(IDictionary<AFAttribute, PIPoint> map,
+                   PIPointList tags,
+                   IDictionary<PIPoint, AFValue> firstRecordedDict,
+                   IDictionary<PIPoint, AFValue> currentValueDict);
 
-    }
 }
